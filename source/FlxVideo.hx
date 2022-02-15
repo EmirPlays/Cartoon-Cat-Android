6897e181e0bfbf71049341b8c3816aba35c96962
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-02-15 21:57:34 +0200

    Create FlxVideo.hx

---
+++ source/FlxVideo.hx
@@ -0,0 +1,37 @@
+#if android
+import extension.webview.WebView;
+import android.*;
+#end
+import flixel.FlxBasic;
+import flixel.FlxG;
+
+class FlxVideo extends FlxBasic {
+	public var finishCallback:Void->Void = null;
+
+	public function new(name:String){
+		super();
+
+		#if android
+		WebView.onClose=onClose;
+		WebView.onURLChanging=onURLChanging;
+		WebView.open(AndroidTools.getFileUrl(name), false, null, ['http://exitme(.*)']);
+                #else//if is not android the game will continue anyway :3
+                if (finishCallback != null){
+			finishCallback();
+		}
+                #end
+	}
+
+	#if android
+	function onClose() {
+		if (finishCallback != null){
+			finishCallback();
+		}
+	 }
+
+	function onURLChanging(url:String){
+		if (url == 'http://exitme/') 
+                        onClose();
+	}
+	#end
+}
