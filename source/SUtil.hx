fc76290d36c4eb57fb4c0bed4149e2dfd5bc2af1
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-02-16 16:37:19 +0200

    Fucking

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -3,12 +3,12 @@ package;
 #if android
 import lime.app.Application;
 import openfl.events.UncaughtErrorEvent;
+import openfl.Lib;
+import sys.FileSystem;
+import sys.io.File;
 import haxe.CallStack.StackItem;
 import haxe.CallStack;
 import haxe.io.Path;
-import sys.FileSystem;
-import sys.io.File;
-import Sys;
 import android.*;
 #end
 
@@ -25,11 +25,11 @@ class SUtil
     	#if android
         if (androidDir != null && androidDir.length > 0) 
         {
-                return androidDir;
+            return androidDir;
         } 
         else 
         {
-                androidDir = storagePath + "/" + Application.current.meta.get("packageName") + "/files/";         
+            androidDir = storagePath + "/" + Application.current.meta.get("packageName") + "/files/";         
         }
         return androidDir;
         #else
@@ -41,73 +41,80 @@ class SUtil
     {
         #if android
         if (!grantedPermsList.contains(Permissions.READ_EXTERNAL_STORAGE) || !grantedPermsList.contains(Permissions.WRITE_EXTERNAL_STORAGE)) {
-                if (AndroidTools.getSDKversion() > 23 || AndroidTools.getSDKversion() == 23) {
-                        AndroidTools.requestPermissions([Permissions.READ_EXTERNAL_STORAGE, Permissions.WRITE_EXTERNAL_STORAGE]);
-                }  
+            if (AndroidTools.getSDKversion() > 23 || AndroidTools.getSDKversion() == 23) {
+                AndroidTools.requestPermissions([Permissions.READ_EXTERNAL_STORAGE, Permissions.WRITE_EXTERNAL_STORAGE]);
+            }  
         }
 
         if (!grantedPermsList.contains(Permissions.READ_EXTERNAL_STORAGE) || !grantedPermsList.contains(Permissions.WRITE_EXTERNAL_STORAGE)) {
-                if (AndroidTools.getSDKversion() > 23 || AndroidTools.getSDKversion() == 23) {
-                        Application.current.window.alert("If you accepted the permisions for storage, good, you can continue, if you not the game can't run without storage permissions please grant them in app settings" + "\n" + "Press Ok To Close The App","Permissions");                        
-                } else {
-                        Application.current.window.alert("game can't run without storage permissions please grant them in app settings" + "\n" + "Press Ok To Close The App","Permissions");                       
-                }
+            if (AndroidTools.getSDKversion() > 23 || AndroidTools.getSDKversion() == 23) {
+                Application.current.window.alert("If you accepted the permisions for storage, good, you can continue, if you not the game can't run without storage permissions please grant them in app settings" + "\n" + "Press Ok To Close The App","Permissions");                        
+            } else {
+                Application.current.window.alert("game can't run without storage permissions please grant them in app settings" + "\n" + "Press Ok To Close The App","Permissions");                       
+            }
         }
 
         if (!FileSystem.exists(storagePath + "/" + Application.current.meta.get("packageName")))
         {
-                FileSystem.createDirectory(storagePath + "/" + Application.current.meta.get("packageName"));
+            FileSystem.createDirectory(storagePath + "/" + Application.current.meta.get("packageName"));
         }
 
         if (!FileSystem.exists(storagePath + "/" + Application.current.meta.get("packageName") + "/files"))
         {
-                FileSystem.createDirectory(storagePath + "/" + Application.current.meta.get("packageName") + "/files");
+            FileSystem.createDirectory(storagePath + "/" + Application.current.meta.get("packageName") + "/files");
         }
 
         if (!FileSystem.exists(SUtil.getPath() + "assets"))
         {
-                Application.current.window.alert("Try copying assets/assets from apk to your internal storage app directory " + "( here " + SUtil.getPath() + " )" + "\n" + "Press Ok To Close The App", "Instructions");
-                Sys.exit(1);//Will close the game
+            Application.current.window.alert("Try copying assets/assets from apk to your internal storage app directory " + "( here " + SUtil.getPath() + " )" + "\n" + "Press Ok To Close The App", "Instructions");
+            Sys.exit(1);
         }
         #end
     }
 
     //Thanks Forever Engine
-    static public function onCrash(e:UncaughtErrorEvent):Void
+    static public function gameCrashCheck()
     {
         #if android
-	var errMsg:String = "";
-	var path:String;
-	var callStack:Array<StackItem> = CallStack.exceptionStack(true);
-	var dateNow:String = Date.now().toString();
-
-	dateNow = StringTools.replace(dateNow, " ", "_");
-	dateNow = StringTools.replace(dateNow, ":", "'");
-
-	path = "gamelog/" + "gamelog_" + dateNow + ".txt";
-
-	for (stackItem in callStack)
-	{
-		switch (stackItem)
-		{
-			case FilePos(s, file, line, column):
-				errMsg += file + " (line " + line + ")\n";
-			default:
-				Sys.println(stackItem);
-		}
-	}
-
-	if (!FileSystem.exists(SUtil.getPath() + "gamelog"))
-		FileSystem.createDirectory(SUtil.getPath() + "gamelog");
-
-	File.saveContent(SUtil.getPath() + path, errMsg + "\n");
-
-	Sys.println(errMsg);
-	Sys.println("Crash log saved in " + Path.normalize(path));
-
-	Application.current.window.alert(errMsg, "Error Found");
-
-	Sys.exit(1);
+    	Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onCrash);
         #end
-     }
+    }
+     
+	static public function onCrash(e:UncaughtErrorEvent):Void
+	{
+		var errMsg:String = "";
+		var path:String;
+		var callStack:Array<StackItem> = CallStack.exceptionStack(true);
+		var dateNow:String = Date.now().toString();
+
+		dateNow = StringTools.replace(dateNow, " ", "_");
+		dateNow = StringTools.replace(dateNow, ":", "'");
+
+		path = "./crash/" + "gameCrash_" + dateNow + ".txt";
+
+		for (stackItem in callStack)
+		{
+			switch (stackItem)
+			{
+				case FilePos(s, file, line, column):
+					errMsg += file + " (line " + line + ")\n";
+				default:
+					Sys.println(stackItem);
+			}
+		}
+
+		if (!FileSystem.exists(SUtil.getPath() + "./crash/"))
+			FileSystem.createDirectory(SUtil.getPath() + "./crash/");
+
+		File.saveContent(SUtil.getPath() + path, errMsg + "\n");
+
+		Sys.println(errMsg);
+		Sys.println("Crash dump saved in " + Path.normalize(path));
+		
+		Sys.println("Making a simple alert ...");
+		
+		Application.current.window.alert(errMsg, "Error Found");
+
+		Sys.exit(1);
+	}  
 }
