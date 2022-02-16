c8771e3b9f85919f3f4854c421873d2d90b5ab2d
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-02-16 13:12:16 +0200

    Update SUtil.hx

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -74,7 +74,7 @@ class SUtil
     }
 
     //Thanks Forever Engine
-    static public function gameCrashCheck()
+    static public function gameCrashCheck():Void
     {
         #if android
     	Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onCrash);
