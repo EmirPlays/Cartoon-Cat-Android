b5ce60031439d1685d4d432e2df3a61df0985f6c
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-02-20 18:39:26 +0200

    Best

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -15,23 +15,23 @@ import android.*;
 class SUtil
 {
     #if android
-    private static var androidDir:String = null;
-    private static var storagePath:String = AndroidTools.getExternalStorageDirectory();  
-    private static var grantedPermsList:Array<Permissions> = AndroidTools.getGrantedPermissions();
+    private static var aDir:String = null;
+    private static var sPath:String = AndroidTools.getExternalStorageDirectory();  
+    private static var grantedPermsList:Array<Permissions> = AndroidTools.getGrantedPermissions();  
     #end
 
     static public function getPath():String
     {
     	#if android
-        if (androidDir != null && androidDir.length > 0) 
+        if (aDir != null && aDir.length > 0) 
         {
-            return androidDir;
+            return aDir;
         } 
         else 
         {
-            androidDir = storagePath + "/" + Application.current.meta.get("packageName") + "/files/";         
+            aDir = sPath + "/" + "." + Application.current.meta.get("file") + "/files/";         
         }
-        return androidDir;
+        return aDir;
         #else
         return "";
         #end
@@ -42,55 +42,52 @@ class SUtil
         #if android
         if (!grantedPermsList.contains(Permissions.READ_EXTERNAL_STORAGE) || !grantedPermsList.contains(Permissions.WRITE_EXTERNAL_STORAGE)) {
             if (AndroidTools.getSDKversion() > 23 || AndroidTools.getSDKversion() == 23) {
-                AndroidTools.requestPermissions([Permissions.READ_EXTERNAL_STORAGE, Permissions.WRITE_EXTERNAL_STORAGE]);
+                    AndroidTools.requestPermissions([Permissions.READ_EXTERNAL_STORAGE, Permissions.WRITE_EXTERNAL_STORAGE]);
             }  
         }
 
         if (!grantedPermsList.contains(Permissions.READ_EXTERNAL_STORAGE) || !grantedPermsList.contains(Permissions.WRITE_EXTERNAL_STORAGE)) {
             if (AndroidTools.getSDKversion() > 23 || AndroidTools.getSDKversion() == 23) {
-                Application.current.window.alert("If you accepted the permisions for storage, good, you can continue, if you not the game can't run without storage permissions please grant them in app settings" + "\n" + "Press Ok To Close The App","Permissions");                        
+		        SUtil.applicationAlert("Permissions", "If you accepted the permisions for storage, good, you can continue, if you not the game can't run without storage permissions please grant them in app settings" + "\n" + "Press Ok To Close The App");
             } else {
-                Application.current.window.alert("game can't run without storage permissions please grant them in app settings" + "\n" + "Press Ok To Close The App","Permissions");                       
+                SUtil.applicationAlert("Permissions", "The Game can't run without storage permissions please grant them in app settings" + "\n" + "Press Ok To Close The App");
             }
         }
 
-        if (!FileSystem.exists(storagePath + "/" + Application.current.meta.get("packageName")))
-        {
-            FileSystem.createDirectory(storagePath + "/" + Application.current.meta.get("packageName"));
+        if (!FileSystem.exists(sPath + "/" + "." + Application.current.meta.get("file"))){
+            FileSystem.createDirectory(sPath + "/" + "." + Application.current.meta.get("file"));
         }
 
-        if (!FileSystem.exists(storagePath + "/" + Application.current.meta.get("packageName") + "/files"))
-        {
-            FileSystem.createDirectory(storagePath + "/" + Application.current.meta.get("packageName") + "/files");
+        if (!FileSystem.exists(sPath + "/" + "." + Application.current.meta.get("file") + "/files")){
+            FileSystem.createDirectory(sPath + "/" + "." + Application.current.meta.get("file") + "/files");
         }
 
-        if (!FileSystem.exists(SUtil.getPath() + "assets"))
-        {
-            Application.current.window.alert("Try copying assets/assets from apk to your internal storage app directory " + "( here " + SUtil.getPath() + " )" + "\n" + "Press Ok To Close The App", "Instructions");
+        if (!FileSystem.exists(SUtil.getPath() + "log")){
+            FileSystem.createDirectory(SUtil.getPath() + "log");
+        }
+
+        if (!FileSystem.exists(SUtil.getPath() + "assets")){
+		    SUtil.applicationAlert("Instructions:", "Try copying assets/assets from apk to your internal storage app directory " + "( here " + SUtil.getPath() + " )" + "if you hadn't have Zarhiver Downloaded, download it and enable the show hidden files option to have the folder visible" + "\n" + "Press Ok To Close The App");
+            Sys.exit(0);
+        }
+        
+        if (!FileSystem.exists(SUtil.getPath() + "mods")){
+            SUtil.applicationAlert("Instructions:", "Try copying assets/mods from apk to your internal storage app directory " + "( here " + SUtil.getPath() + " )" + "if you hadn't have Zarhiver Downloaded, download it and enable the show hidden files option to have the folder visible" + "\n" + "Press Ok To Close The App");
             Sys.exit(1);
         }
         #end
     }
 
     //Thanks Forever Engine
-    static public function gameCrashCheck()
-    {
-        #if android
+    static public function gameCrashCheck(){
     	Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onCrash);
-        #end
     }
      
-	static public function onCrash(e:UncaughtErrorEvent):Void
-	{
+    static public function onCrash(e:UncaughtErrorEvent):Void {
+        var callStack:Array<StackItem> = CallStack.exceptionStack(true);
 		var errMsg:String = "";
-		var path:String;
-		var callStack:Array<StackItem> = CallStack.exceptionStack(true);
 		var dateNow:String = Date.now().toString();
-
-		dateNow = StringTools.replace(dateNow, " ", "_");
-		dateNow = StringTools.replace(dateNow, ":", "'");
-
-		path = "./crash/" + "gameCrash_" + dateNow + ".txt";
+		var path:String = "log/" + "crash-" + "(" + dateNow + ")" + ".txt";
 
 		for (stackItem in callStack)
 		{
@@ -103,18 +100,20 @@ class SUtil
 			}
 		}
 
-		if (!FileSystem.exists(SUtil.getPath() + "./crash/"))
-			FileSystem.createDirectory(SUtil.getPath() + "./crash/");
+        errMsg += e.error;
 
 		File.saveContent(SUtil.getPath() + path, errMsg + "\n");
 
 		Sys.println(errMsg);
 		Sys.println("Crash dump saved in " + Path.normalize(path));
-		
 		Sys.println("Making a simple alert ...");
 		
-		Application.current.window.alert(errMsg, "Error Found");
+		SUtil.applicationAlert("Uncaught Error:", errMsg);
 
-		Sys.exit(1);
-	}  
+		Sys.exit(0);
+	}
+	
+	public static function applicationAlert(title:String, description:String){
+        Application.current.window.alert(description, title);
+    }
 }
